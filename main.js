9eca464ed60adaf780666f97c7b89c9d0c87fbc5
Dmitry <bystrodmitry@gmail.com>
2022-09-24 15:53:35 +0300

    first App

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 "use strict";
 
-
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const personMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: fslse
+};
