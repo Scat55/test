ed909a33ac7dd0ac038905d190d758bbcff06ea1
Dmitry <bystrodmitry@gmail.com>
2022-09-24 17:05:34 +0300

    add App 2.0

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
 const personMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: fslse
+  privat: false
 };
+
+const a = prompt("Один из последных просмотренных фильмов?", ""),
+      b = prompt("На сколько оцениете его?", ""),
+      c = prompt("Один из последных просмотренных фильмов?", ""),
+      d = prompt("На сколько оцениете его?", "");
+
+personMovieDB.movies[a] = b;
+personMovieDB.movies[c] = d;
+console.log(personMovieDB.movies);
