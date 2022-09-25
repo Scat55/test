b9d36b29dfacc94c196b936cdf5f7397424706c0
Dmitry <bystrodmitry@gmail.com>
2022-09-25 20:07:37 +0300

    -aApp 3.0

--- main.js
+++ main.js
@@ -10,11 +10,29 @@ const personMovieDB = {
   privat: false
 };
 
-const a = prompt("Один из последных просмотренных фильмов?", ""),
-      b = prompt("На сколько оцениете его?", ""),
-      c = prompt("Один из последных просмотренных фильмов?", ""),
-      d = prompt("На сколько оцениете его?", "");
+for (let i = 0; i < 2; i++){
+  const a = prompt("Один из последных просмотренных фильмов?", ""),
+        b = prompt("На сколько оцениете его?", "");
 
-personMovieDB.movies[a] = b;
-personMovieDB.movies[c] = d;
-console.log(personMovieDB.movies);
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personMovieDB.movies[a] = b;
+    console.log('Done!');
+  } else {
+    console.log('Error');
+    i--;
+    
+  }
+}
+if(personMovieDB.count < 10){
+  alert('Вы посмотрели мало фильмов');
+} else if(personMovieDB.count >= 10 && personMovieDB.count < 30 ){
+  alert('Вы классический зритель!');
+}else if(personMovieDB.count >= 30){
+  alert('Вы киноман!');
+}else{
+  alert('Произошла ошибка!');
+}
+
+
+console.log(personMovieDB);
+
